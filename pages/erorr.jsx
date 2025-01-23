4abc9cd1c3b68b53bc78a85ed8932be67a4fce27
reokook <amlalhlaly668@gmail.com>
2025-01-23 20:40:43 +0300

    commit

---
+++ pages/erorr.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
